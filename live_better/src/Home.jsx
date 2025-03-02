5b06b56f2bfa9af83ea4a06eaf3f847d924563a8
Darkzlyn ** <you@example.comcb221243@qmul.ac.uk>
2025-03-02 15:13:28 +0000

    Handling the selection of the city added to Home.jsx

--- live_better/src/Home.jsx
+++ live_better/src/Home.jsx
@@ -1,11 +1,33 @@
 import React, { useState } from "react";
 import CitySearch from "./components/CitySearch";
 
+import { getCoordinatesByCity } from "./api/geocodingUtils";
+
 const Home = () => {
+
+    const [selectedCity, setSelectedCity] = useState(null);
+    const [loading, setLoading] = useState(null);
+    const [coordinates, setCoordinates] = useState(null);
+
+    
+
+    const handleCitySelect = async (cityName) => {
+        setLoading(true);
+        setSelectedCity(cityName);
+
+        // Fetch and set coordinates
+        const locationData = await getCoordinatesByCity(cityName);
+        if (locationData) {
+          setCoordinates({ lat: locationData.lat, lon: locationData.lon });
+        }
+    
+        setLoading(false);
+      };
+    
     return (
         <div className="w-screen text-black min-h-screen flex flex-col items-center justify-center bg-white px-6 md:px-12">
 
-          <CitySearch />
+          <CitySearch onSelectCity = {handleCitySelect}/>
 
         </div>
       );
