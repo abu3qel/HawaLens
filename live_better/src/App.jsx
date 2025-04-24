a9b1821144d36746673cbd5001524cc26b0ad023
nbm-305 <158157397+nbm-305@users.noreply.github.com>
2025-04-24 02:41:20 +0100

    event context for email triggering (suod)

--- live_better/src/App.jsx
+++ live_better/src/App.jsx
@@ -6,31 +6,32 @@ import Header from './components/header';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Footer from './components/footer';
 import { AuthProvider } from './components/AuthContext';
-import React from 'react';
-
-// import Home from './Home';
 import LocationDetailPage from './pages/LocationDetailPage';
 import AQIMapComponent from './components/aqiMap';
+import { EventProvider } from './context/EventContext';
+
 function App() {
   return (
     <AuthProvider> 
-      <Router>
-        <div className="min-h-screen flex flex-col bg-white">
-          <Header /> 
-          
-          <main className="flex-grow">
-            <Routes>
-              <Route path="/" element={<AQIMapComponent />} />
-              <Route path="/map" element={<AQIMapComponent />} />
-              <Route path="/location/:lat/:lon" element={<LocationDetailPage />} />
-            </Routes>
-          </main>
+      <EventProvider>
+        <Router>
+          <div className="min-h-screen flex flex-col bg-white">
+            <Header /> 
+            
+            <main className="flex-grow">
+              <Routes>
+                <Route path="/" element={<AQIMapComponent />} />
+                <Route path="/map" element={<AQIMapComponent />} />
+                <Route path="/location/:lat/:lon" element={<LocationDetailPage />} />
+              </Routes>
+            </main>
 
-          <Footer /> 
-        </div>
-      </Router>
+            <Footer /> 
+          </div>
+        </Router>
+      </EventProvider>
     </AuthProvider>
   );
 }
 
-export default App
+export default App
